1ac5f83afd42632112a85d879426ef331d9a0df1
Naveen23-12 <naveenlakshmi2312@gmail.com>
2025-09-13 19:05:07 +0530

    Second commit

--- loopingTasks/DoWhileLoopTask15.java
+++ loopingTasks/DoWhileLoopTask15.java
@@ -3,16 +3,16 @@ package loopingTasks;
 //Print Armstrong numbers between 1 and 500.
 public class DoWhileLoopTask15 {
 	public static void main(String[] args) {
-		int n = 1, digit = 0, rem;
+		int n = 1, rem;
 		do {
 			int i=n;
 			int temp=n;
-			int sum=0;	
+			int sum=0;
 			do {
 				rem = temp % 10;
 				sum+=rem*rem*rem;
 				temp=temp/10;
-				digit++;
+				
 			} while (temp > 0);
 			if(n==sum) {
 				System.out.println(sum);
--- loopingTasks/WhileLoopTask13.java
+++ loopingTasks/WhileLoopTask13.java
@@ -3,10 +3,11 @@ package loopingTasks;
 //Print all perfect numbers between 1 and 1000.
 public class WhileLoopTask13 {
 	public static void main(String[] args) {
-		int i, j = 1, rem;
-		for (i=1; i<= 1000;i++) {
+		int i, rem;
+		for (i = 1; i <= 1000; i++) {
 			int sum = 0;
-			while (j <= i / 2) {	
+			int j = 1;
+			while (j <= i / 2) {
 				if (i % j == 0) {
 					sum = sum + j;
 				}
